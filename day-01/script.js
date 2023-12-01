f57d170196f84c769ea3724d553663b218371473
cryptbby <150791349+cryptbby@users.noreply.github.com>
2023-12-01 13:26:44 +0000

    refactor

--- day-01/script.js
+++ day-01/script.js
@@ -2,20 +2,17 @@ const fs = require('fs');
 
 let data = fs.readFileSync('input.data', 'utf8');
 let strings = data.split('\n');
-let result = 0;
 
-const day1 = () => {
-    let numbers = strings.map(string => {
-        let nums = string.match(/\d+/g).join('');
-        return Number(nums.charAt(0) + nums.charAt(nums.length - 1));
-    });
-    
-    for (let i = 0; i < numbers.length; i++) {
-        result += numbers[i];
+const trebuchet = () => {
+    let result = 0;
+
+    for (let i = 0; i < strings.length; i++) {
+        let nums = strings[i].match(/\d+/g).join('');
+        let number = Number(nums.charAt(0) + nums.charAt(nums.length - 1));
+        result += number;
     }
 
     console.log(result);
 }
 
-
-day1();
+trebuchet();
