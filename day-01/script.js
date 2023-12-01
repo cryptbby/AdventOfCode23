894bc2a5e914d045d296f8e5f6732e1e984418e9
cryptbby <150791349+cryptbby@users.noreply.github.com>
2023-12-01 13:24:58 +0000

    added function

--- day-01/script.js
+++ day-01/script.js
@@ -4,13 +4,18 @@ let data = fs.readFileSync('input.data', 'utf8');
 let strings = data.split('\n');
 let result = 0;
 
-let numbers = strings.map(string => {
-    let nums = string.match(/\d+/g).join('');
-    return Number(nums.charAt(0) + nums.charAt(nums.length - 1));
-});
+const day1 = () => {
+    let numbers = strings.map(string => {
+        let nums = string.match(/\d+/g).join('');
+        return Number(nums.charAt(0) + nums.charAt(nums.length - 1));
+    });
+    
+    for (let i = 0; i < numbers.length; i++) {
+        result += numbers[i];
+    }
 
-for (let i = 0; i < numbers.length; i++) {
-    result += numbers[i];
+    console.log(result);
 }
 
-console.log(result);
+
+day1();
